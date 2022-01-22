2cce511f9dbbc3bc39f6e51e15d578684658f3c4
jfran192 <jfran192@ford.com>
2022-01-22 10:18:30 -0300

    Initial Commit.

---
+++ hero/src/test/java/com/jjdev/hero/HeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjdev.hero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
